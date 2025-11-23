ef4d2c44b3e8dc15d8788bbea676598d6c0c6222
h0p3zZ <h0p3zZ@outlook.com>
2025-11-23 23:57:30 +0100

    reduced nesting in SolutionVerifier.cs

--- _prog/SolutionVerifier/SolutionVerifier.cs
+++ _prog/SolutionVerifier/SolutionVerifier.cs
@@ -25,74 +25,72 @@ internal class SolutionVerifier
         {
             var task1 = _problem.Tasks[assign1.Key];
 
-            if (assign1.Value is { } task1TimeSlot)
+            if (assign1.Value is not { } task1TimeSlot) {
+                scheduleCost.ImportanceCost += task1.Importance;
+                continue;
+            }
+
+            scheduleCost.UrgencyCost += (task1.Urgency + 1) * task1TimeSlot;
+            // Check whether the task has exceeded the scheduling horizon
+            if (task1TimeSlot + task1.Duration >= _problem.Horizon)
             {
-                scheduleCost.UrgencyCost += (task1.Urgency + 1) * task1TimeSlot;
-                // Check whether the task has exceeded the scheduling horizon
-                if (task1TimeSlot + task1.Duration >= _problem.Horizon)
+                Console.WriteLine($"Task {assign1.Key} exceeds the scheduling horizon.");
+                return scheduleCost;
+            }
+
+            foreach (var assign2 in _assignments)
+            {
+                if (assign2.Value is not { } task2TimeSlot) continue;
+                if (assign1.Key == assign2.Key) continue;
+
+                var task2 = _problem.Tasks[assign2.Key];
+                // Check whether tasks overlap with one another
+                if (
+                    task1TimeSlot + task1.Duration > task2TimeSlot && task2TimeSlot + task2.Duration > task1TimeSlot
+                )
                 {
-                    Console.WriteLine($"Task {assign1.Key} exceeds the scheduling horizon.");
+                    Console.WriteLine($"Tasks {assign1.Key} and {assign2.Key} overlap with one another.");
                     return scheduleCost;
                 }
 
-                foreach (var assign2 in _assignments)
+                // Check dependency cost
+                foreach (var dependency in task2.Dependencies ?? [])
                 {
-                    if (assign2.Value is not { } task2TimeSlot) continue;
-                    if (assign1.Key == assign2.Key) continue;
+                    if (dependency.Task != assign1.Key)
+                        continue;
 
-                    var task2 = _problem.Tasks[assign2.Key];
-                    // Check whether tasks overlap with one another
-                    if (
-                        task1TimeSlot + task1.Duration > task2TimeSlot && task2TimeSlot + task2.Duration > task1TimeSlot
-                    )
+                    var intervals = dependency.Intervals.Where(x =>
+                        task2TimeSlot - task1TimeSlot + task1.Duration > x.Interval[0] &&
+                        task2TimeSlot - task1TimeSlot + task1.Duration < x.Interval[1]
+                    );
+                    if (intervals.Any())
                     {
-                        Console.WriteLine($"Tasks {assign1.Key} and {assign2.Key} overlap with one another.");
-                        return scheduleCost;
-                    }
+                        double weightedSum = 0;
+                        double totalOverlap = 0;
 
-                    // Check dependency cost
-                    foreach (var dependency in task2.Dependencies ?? [])
-                    {
-                        if (dependency.Task == assign1.Key)
+                        foreach (var interval in intervals)
                         {
-                            var intervals = dependency.Intervals.Where(x =>
-                                task2TimeSlot - task1TimeSlot + task1.Duration > x.Interval[0] &&
-                                task2TimeSlot - task1TimeSlot + task1.Duration < x.Interval[1]
-                            );
-                            if (intervals.Any())
+                            var intervalStart = interval.Interval[0];
+                            var intervalEnd = interval.Interval[1];
+
+                            // actual overlap between the task window and the interval
+                            var overlapStart = Math.Max(task2TimeSlot, intervalStart);
+                            var overlapEnd = Math.Min(task2TimeSlot + task2.Duration, intervalEnd);
+                            var overlapLength = overlapEnd - overlapStart;
+
+                            if (overlapLength > 0)
                             {
-                                double weightedSum = 0;
-                                double totalOverlap = 0;
-
-                                foreach (var interval in intervals)
-                                {
-                                    var intervalStart = interval.Interval[0];
-                                    var intervalEnd = interval.Interval[1];
-
-                                    // actual overlap between the task window and the interval
-                                    var overlapStart = Math.Max(task2TimeSlot, intervalStart);
-                                    var overlapEnd = Math.Min(task2TimeSlot + task2.Duration, intervalEnd);
-                                    var overlapLength = overlapEnd - overlapStart;
-
-                                    if (overlapLength > 0)
-                                    {
-                                        weightedSum += overlapLength * interval.Cost;
-                                        totalOverlap += overlapLength;
-                                    }
-                                }
-
-                                var weightedCost = weightedSum / totalOverlap;
-                                scheduleCost.DependencyCost += weightedCost;
+                                weightedSum += overlapLength * interval.Cost;
+                                totalOverlap += overlapLength;
                             }
-                            else
-                                scheduleCost.DependencyCost += dependency.UnmetCost;
                         }
+
+                        var weightedCost = weightedSum / totalOverlap;
+                        scheduleCost.DependencyCost += weightedCost;
                     }
+                    else
+                        scheduleCost.DependencyCost += dependency.UnmetCost;
                 }
-            }
-            else
-            {
-                scheduleCost.ImportanceCost += task1.Importance;
             }
         }
 
