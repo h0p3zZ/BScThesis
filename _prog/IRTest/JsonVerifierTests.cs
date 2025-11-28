7dd3f99f454ff70077f8186be7f8972186e75157
h0p3zZ <h0p3zZ@outlook.com>
2025-11-28 21:10:23 +0100

    added JsonVerifier tests for existing json schedules

--- _prog/IRTest/JsonVerifierTests.cs
+++ _prog/IRTest/JsonVerifierTests.cs
@@ -1,10 +1,47 @@
-﻿namespace VerfifierTests;
+﻿using JsonVerifier;
+
+namespace VerfifierTests;
 
 [TestClass]
 public sealed class JsonVerifierTests
 {
     [TestMethod]
-    public void TestMethod1()
+    [DataRow("ExampleSchedules/generated1.json")]
+    [DataRow("ExampleSchedules/generated2.json")]
+    [DataRow("ExampleSchedules/generated3.json")]
+    [DataRow("ExampleSchedules/generated4.json")]
+    [DataRow("ExampleSchedules/generated5.json")]
+    [DataRow("ExampleSchedules/generated6.json")]
+    [DataRow("ExampleSchedules/generated7.json")]
+    [DataRow("ExampleSchedules/schedule.json")]
+    [DataRow("ExampleSchedules/validSchedule.json")]
+    public void TestValidJson(string filePath)
     {
+        var verifier = new JsonParser(filePath);
+
+        var isValid = verifier.ValidateSchema(out var messages);
+
+        foreach (var message in messages)
+        {
+            Console.WriteLine(message);
+        }
+
+        Assert.IsTrue(isValid, "Expected JSON to be valid.");
+    }
+
+    [TestMethod]
+    [DataRow("ExampleSchedules/invalidSchedule.json")]
+    public void TestInvalidJson(string filePath)
+    {
+        var verifier = new JsonParser(filePath);
+
+        var isValid = verifier.ValidateSchema(out var messages);
+
+        foreach (var message in messages)
+        {
+            Console.WriteLine(message);
+        }
+
+        Assert.IsFalse(isValid, "Expected JSON to be invalid.");
     }
 }
